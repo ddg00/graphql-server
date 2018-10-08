bf60027be9020dca1f3deb2f43d6853bf07d2e81
arya utama putra <arya.ddg00@gmail.com>
2018-10-08 16:50:14 +0700

    graphql server prototype

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,8 +1,11 @@
 import { QueryResolvers } from '../generated/resolvers'
+import { getUserId } from '../utils'
 import { TypeMap } from './types/TypeMap'
 
 export interface QueryParent {}
 
 export const Query: QueryResolvers.Type<TypeMap> = {
-  me: parent => null,
+  me: (_parent, _args, ctx) => {
+    return ctx.db.user({ id: getUserId(ctx) })
+  },
 }
